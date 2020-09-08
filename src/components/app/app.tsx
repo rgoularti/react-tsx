2b6be710bb22d74e0c0c3f867250f68020353b2b
Rodrigo Goulart Ribeiro <rodrigo.goulart.ribeiro@gmail.com>
2020-09-08 18:29:05 -0300

    Add header and footer to app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,36 @@
 import React, { ReactElement } from 'react';
 
 import { Button, Section, TextField } from '../../shared';
+import styles from './app.module.css';
 
 export class App extends React.Component {
   render(): ReactElement {
     return (
-      <Section>
-        <TextField id="name" labelText="Enter your name:"></TextField>
-        <Button primary>Submit</Button>
-      </Section>
+      <main>
+        <section className="hero">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">React with Typescript</h1>
+              <h2 className="subtitle">Sample project for study purposes</h2>
+            </div>
+          </div>
+        </section>
+
+        <div className={`container ${styles.content}`}>
+          <Section>
+            <TextField id="name" labelText="Enter your name:"></TextField>
+            <Button primary>Submit</Button>
+          </Section>
+        </div>
+
+        <footer className="footer">
+          <div className="content has-text-centered">
+            <p>
+              <strong>Created</strong> by <a href="https://www.linkedin.com/in/rgoularti">Rodrigo Goulart</a>.
+            </p>
+          </div>
+        </footer>
+      </main>
     );
   }
 }
